65065d5e256ba8965cb4eb2830a19261d511c6b9
yuni0719 <nicolewu0719@gmail.com>
2024-09-12 16:21:16 +0800

    week01

---
+++ week01/week01_4a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	
+	cout << n/16 %2;
+	cout << n/8 %2;
+	cout << n/4 %2;
+	cout << n/2 %2;
+	cout << n%2;
+	return 0;
+}
