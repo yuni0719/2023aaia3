1332df5b1974a78dcb566d0ebb54923fb97272b4
yuni0719 <nicolewu0719@gmail.com>
2024-11-07 15:22:42 +0800

    week09

--- week09/week09_2.c
+++ week09/week09_2.c
@@ -1,7 +1,5 @@
-class Solution {
-public:
-    int diagonalSum(vector<vector<int>>& mat) {
-        int M = mat.size();
+int diagonalSum(int** mat, int matSize, int* matColSize) {
+    int M = matSize;
         int ans = 0;
         for(int i=0; i<M; i++) {
             ans += mat[i][i]; //左上、右下
@@ -10,5 +8,4 @@ public:
         //單數時, 要扣掉中間(兩個叉叉的焦點)
         if(M%2==1) ans -= mat[M/2][M/2];
         return ans;
-    }
-};
+}
