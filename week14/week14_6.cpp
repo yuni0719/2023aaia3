38acd2de75c38efbc0b316180a2696320328f5a2
yuni0719 <nicolewu0719@gmail.com>
2024-12-12 15:35:36 +0800

    week14

---
+++ week14/week14_6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b; 
+	while(cin >> a >> b) {
+		int bad = 0, backup = a;
+		while(a>1) {
+			if(a%b>0) bad = 1;
+			a = a / b;
+		
+		}
+		if(bad==1) cout << "Boring!\n";
+		else {
+			a = backup;
+			while(a>0) { 
+				cout << a << " ";
+				a = a / b;
+			}
+			cout << "\n";
+		}
+	} 
+}
+		
