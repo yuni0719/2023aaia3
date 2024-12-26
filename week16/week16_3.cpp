16459799f50a7806d3422e3cf7085af7f00b2507
yuni0719 <nicolewu0719@gmail.com>
2024-12-26 15:01:01 +0800

    week16

--- week16/week16_3.cpp
+++ week16/week16_3.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    double helper(double x, int n) {
+    double helper(double x, int n) { //函式呼叫函式
         if(n==0) return 1;
-        if(n==1) return x;
         double now = helper(x, n/2);
         if(n%2==0) return now * now;
-        else return now * now * x;
+        else return now * now * x; //奇數, 多成一個x
     }
-    double myPow(double x, int n) {
+    double myPow(double x, long long n) { //要改成很長、很長的整數
         double ans = 1; 
         if(n<0) { //遇到父的會失敗, 所以要把n變成正的
             n = -n; //n變成正的
             x = 1/x; //-1次方, 就是1/x
         }
-        for(int i=0; i<n; i++) { //不幸的是, 會失敗 遇到「婦的n」會失敗
-            ans *= x;
-        }
-        return ans; 
+        return helper(x, n); 
     }
 };
