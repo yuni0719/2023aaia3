856445cca3306992fd7621bfb99420e18ab52fa4
yuni0719 <nicolewu0719@gmail.com>
2025-01-02 15:59:58 +0800

    week17

---
+++ week17/week17_3a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string a, b;
+	cin >> a;
+	cin >> b;
+	int N1 = a.length(), N2 = b.length();
+	if(N1>N2) cout << 1;
+	else if(N1<N2) cout << -1;
+	else {
+		cout << a.compare(b);
+	}
+}
